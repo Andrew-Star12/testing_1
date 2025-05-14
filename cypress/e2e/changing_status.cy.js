4671f6261fd5c6716cbbcdead8b5994b78260a66
Andrew-Star12 <andrejstarovojtov22@gmail.com>
2025-05-14 10:44:13 +0700

    Негативное создание потребности

--- cypress/e2e/changing_status.cy.js
+++ cypress/e2e/changing_status.cy.js
@@ -1,41 +1,40 @@
-describe('Смена статуса потребности', () => {
-    it('Работодатель меняет статус потребности', () => {
-        // 1. Авторизация
-        cy.visit('https://dev.profteam.su/login');
+    describe('Смена статуса потребности', () => {
+        it('Работодатель меняет статус потребности', () => {
+            // 1. Авторизация
+            cy.visit('https://dev.profteam.su/login');
 
-        cy.get('input[type=text]').type('testerEmployer');
-        cy.get('input[type=password]').type('Password1');
-        cy.get('button[type=submit]').eq(2).click();
+            cy.get('input[type=text]').type('testerEmployer');
+            cy.get('input[type=password]').type('Password1');
+            cy.get('button[type=submit]').eq(2).click();
 
-        cy.url().should('include', '/account/main');
+            cy.url().should('include', '/account/main');
 
-        // Открыть раздел "Потребности"
-        cy.get(':nth-child(6) > .menu-item__item-name')
-            .should('be.visible')
-            .click();
+            // Открыть раздел "Потребности"
+            cy.get(':nth-child(6) > .menu-item__item-name')
+                .should('be.visible')
+                .click();
 
-        // Подождать загрузку фильтров
-        cy.get('.needs-block__filters-wrapper', { timeout: 10000 }).should('be.visible');
+            // Подождать загрузку фильтров
+            cy.get('.needs-block__filters-wrapper', { timeout: 10000 }).should('be.visible');
 
-        //  Добавляем задержку 1 секунда
-        cy.wait(1000);
+            //  Добавляем задержку 1 секунда
+            cy.wait(1000);
 
-        // Второй фильтр — клик и выбор
-        cy.get('.needs-block__filters-wrapper > div > div > div')
-            .first()
-            .click({ force: true });
+            cy.get('.needs-block__filters-wrapper > div > div > div')
+                .first()
+                .click({ force: true });
 
-        cy.get('.form-select__items > div')
-            .eq(1)
-            .should('be.visible')
-            .click({ force: true });
+            cy.get('.form-select__items > div')
+                .eq(1)
+                .should('be.visible')
+                .click({ force: true });
 
-        // Подождать появление карточек
-        cy.get('.infinite-loader.need-list > div', { timeout: 10000 }).should('have.length.greaterThan', 0);
+            // Подождать появление карточек
+            cy.get('.infinite-loader.need-list > div', { timeout: 10000 }).should('have.length.greaterThan', 0);
 
-        // Клик на кнопку в первой карточке
-        cy.get('.infinite-loader.need-list > div:nth-child(1) .need-footer__button-wrapper > button.button__background-color-green')
-            .should('be.visible')
-            .click({ force: true });
+            // Клик на кнопку в первой карточке
+            cy.get('.infinite-loader.need-list > div:nth-child(1) .need-footer__button-wrapper > button.button__background-color-green')
+                .should('be.visible')
+                .click({ force: true });
+        });
     });
-});
--- cypress/e2e/create_need_negative.cy.js
+++ cypress/e2e/create_need_negative.cy.js
@@ -1,25 +1,20 @@
-describe('Создание потребности', () => {
-    it('Работодатель авторизуется и создает потребность', () => {
-
+describe('Смена статуса потребности', () => {
+    it('Не удается создать потребность без названия', () => {
         cy.visit('https://dev.profteam.su/login');
-
         cy.get('input[type=text]').type('testerInstitution');
         cy.get('input[type=password]').type('Password1');
         cy.get('button[type=submit]').eq(2).click();
-
         cy.get(':nth-child(6) > .menu-item__item-name').click();
-        cy.get('[data-v-99600bb6=""][data-v-4849dea2=""] > .needs-block > .needs-block__content > .needs-block__filters-wrapper > .button').click();
+        cy.get('[data-v-99600bb6=""] > .needs-block__content > .needs-block__filters-wrapper > .button').click();
 
         cy.get('.vacancy-need-wrapper > .form').should('be.visible');
-        // cy.get(' body > div:nth-child(17) > div.desktop-modal > div > div.vacancy-need-wrapper > form > div:nth-child(1) > div.form__labels > div > div:nth-child(1) > div > input').type('Разнорабочий', {force: true});
-        cy.get('.vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(1) > .form-control--responsive > .form-input--text',{ timeout: 10000 }).first().type('Разнорабочий', {force: true});
+
+        // Не заполняем название
+        // Остальные поля как обычно
         cy.get('.vacancy-need-wrapper > .form > :nth-child(1) > .form__labels > .labels > :nth-child(2) > .salary-field > .salary-field__wrapper--bottom > .radio-list > :nth-child(2)').click({ multiple: true, force: true });
         cy.get('.vacancy-need-wrapper > form > div:nth-child(1) > div.form__labels > div > div:nth-child(3) > div > textarea[placeholder="Обязанности сотрудника"]',{ timeout: 10000 }) .eq(0).should('be.visible').type(',,,,,,,,jjjjjй', {force: true});
         cy.get('.vacancy-need-wrapper > form > div:nth-child(1) > div.form__labels > div > div:nth-child(4) > div > textarea[placeholder="Ваши требования"]',{ timeout: 10000 }) .eq(0).should('be.visible').type(',,,,,,,,jjjjjй', {force: true});
 
-        // cy.get('.form-select__selected').click({ force: true });
-        // cy.contains('.form-select__option', 'Дистант').eq(1).click({multiple: true, force: true });
-
-        cy.get('.vacancy-need-wrapper > .form > .form__buttons > .button').eq(1).click({force: true});
+        cy.get('.vacancy-need-wrapper > .form > .form__buttons > .button').eq(0).click();
     });
 });
